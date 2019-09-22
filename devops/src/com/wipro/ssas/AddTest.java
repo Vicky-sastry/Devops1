41e18e838585fd23d0ffc8b29d8db136dc25dd95
user <user@192.168.10.12>
2019-09-22 20:24:11 +0530

    Added test case

---
+++ devops/src/com/wipro/ssas/AddTest.java
@@ -0,0 +1,17 @@
+package com.wipro.ssas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddTest {
+
+	@Test
+	public void AddTestt() {
+		devops junit=new devops();
+		int result junit.add(100,500);
+	    assertEquals(600,actual);
+		
+	}
+
+}
--- devops/src/com/wipro/ssas/devops.java
+++ devops/src/com/wipro/ssas/devops.java
@@ -2,17 +2,11 @@ package com.wipro.ssas;
 import java.util.Scanner;
 public class devops {
 
-	public static void main(String[] args) {
+	public static int main(String[] args) {
 		// TODO Auto-generated method stub
-		int num1,num2,sum;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter First Number: ");
-        num1=sc.nextInt();
-        System.out.println("Enter Second Number: ");
-        num2=sc.nextInt();
-        sum=num1+num2;
-		System.out.println("Sum is: " +sum);
-
+		public int add(int a,int b) {
+			return a+b;
+		}
 	}
 
 }
