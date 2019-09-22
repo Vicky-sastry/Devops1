38348a9fcefb222e61d3c32170b8974b8d1be1fd
user <user@192.168.10.12>
2019-09-22 16:43:39 +0530

    First Commit

---
+++ devops/src/com/wipro/pr1/devops1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+package com.wipro.pr1;
+public class devops1 {
+	public static void main(String[] args)
+	{
+		
+        System.out.println("Sum of these numbers:");
+
+	}
+
+}
---
+++ devops/src/com/wipro/ssas/devops.java
@@ -0,0 +1,18 @@
+package com.wipro.ssas;
+import java.util.Scanner;
+public class devops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2,sum;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        num1=sc.nextInt();
+        System.out.println("Enter Second Number: ");
+        num2=sc.nextInt();
+        sum=num1+num2;
+		System.out.println("Sum is: " +sum);
+
+	}
+
+}
